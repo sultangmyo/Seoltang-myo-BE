f39b7a071176e9e8b4a858e804d349184d959a26
5ranke <wjdtnwls0106@gmail.com>
2026-04-08 14:07:17 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/seoltangmyo/sugarcat/SugarcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoltangmyo.sugarcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugarcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
