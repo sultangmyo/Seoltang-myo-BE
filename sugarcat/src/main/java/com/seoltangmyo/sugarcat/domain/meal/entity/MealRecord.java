2a94b441fb4406afeea74eb09640991029648138
meaningGitt <a0104037a@gmail.com>
2026-04-12 00:13:50 +0900

    feature: MealRecord entity 추가

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/meal/entity/MealRecord.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/meal/entity/MealRecord.java
@@ -1,4 +1,49 @@
 package com.seoltangmyo.sugarcat.domain.meal.entity;
 
+import com.seoltangmyo.sugarcat.domain.cat.entity.Cat;
+import com.seoltangmyo.sugarcat.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "meal_records")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class MealRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID mealRecordId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cat_id", nullable = false)
+    private Cat cat;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recorded_by_user_id")
+    private User recordedBy;
+
+    @Column(name = "record_date", nullable = false)
+    private LocalDate recordDate;
+
+    @Column(name = "record_time", nullable = false)
+    private LocalTime recordTime;
+
+    @Column(name = "sequence", nullable = false)
+    private int sequence;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "meal_status", length = 20, nullable = false)
+    private MealStatus mealStatus;
+
+
+
+
 }
