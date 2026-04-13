6a555e908becbd5a9c1e34169f828b81feb26b33
5ranke <wjdtnwls0106@gmail.com>
2026-04-13 18:41:12 +0900

    feat: SecurityConfig 설정

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/global/config/SecurityConfig.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/global/config/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.seoltangmyo.sugarcat.global.config;
 
+import com.seoltangmyo.sugarcat.global.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // 추후에 csrf 프론트만 열어야함.
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
