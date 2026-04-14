e1de5b1935d2a6758de026291089ff3644e8e05c
5ranke <wjdtnwls0106@gmail.com>
2026-04-15 03:04:26 +0900

    style: 사용 안하는 import문 삭제

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/auth/controller/AuthController.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/auth/controller/AuthController.java
@@ -8,7 +8,6 @@ import com.seoltangmyo.sugarcat.domain.auth.service.AuthService;
 import com.seoltangmyo.sugarcat.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
