a104bc46eb73a3b9e63bb6133d609e6e50f6745d
5ranke <wjdtnwls0106@gmail.com>
2026-04-16 15:12:54 +0900

    feat: 스키마 파일 not null 수정

--- sugarcat/src/main/resources/schema.sql
+++ sugarcat/src/main/resources/schema.sql
@@ -58,7 +58,7 @@ CREATE TABLE users
     insulin_notification_enabled     BOOLEAN     NOT NULL DEFAULT FALSE,
     blood_sugar_notification_enabled BOOLEAN     NOT NULL DEFAULT FALSE,
     meal_notification_enabled        BOOLEAN     NOT NULL DEFAULT FALSE,
-    provider                         provider_type_enum NULL,
+    provider                         provider_type_enum NOT NULL,
     provider_id                      VARCHAR(100) NOT NULL,
     refresh_token                    VARCHAR(500),
     refresh_token_expires_at         TIMESTAMPTZ,
