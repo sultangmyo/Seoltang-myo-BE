0c62e82dfb422029b130c39ac86bced59d8ae805
5ranke <wjdtnwls0106@gmail.com>
2026-05-04 19:21:48 +0900

    refactor: KakaoUserInfoResponse 역직렬화 에러 방어코드 추가

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/auth/dto/KakaoUserInfoResponse.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/auth/dto/KakaoUserInfoResponse.java
@@ -1,5 +1,8 @@
 package com.seoltangmyo.sugarcat.domain.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record KakaoUserInfoResponse(
         Long id
         // 카카오에서 내려주는 회원 고유 id
