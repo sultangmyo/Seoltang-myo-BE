8ddfaffa79a8ccf9635b67155ccc2fdcbc2a9183
5ranke <wjdtnwls0106@gmail.com>
2026-05-05 15:07:51 +0900

    feat: provider+providerId로 신규유저 생성 로직 추가

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/user/entity/User.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/user/entity/User.java
@@ -75,4 +75,20 @@ public class User {
         this.refreshTokenExpiresAt = null;
     }
 
+    public static User createSocialUser(ProviderType provider, String providerId) {
+        User user = new User();
+
+        user.provider = provider;
+        user.providerId = providerId;
+
+        user.insulinNotiEnabled = false;
+        user.bloodSugarNotiEnabled = false;
+        user.mealNotiEnabled = false;
+        user.weeklyReportNotificationEnabled = false;
+
+        // onboardingCompleted 추가해야함
+
+        return user;
+    }
+
 }
