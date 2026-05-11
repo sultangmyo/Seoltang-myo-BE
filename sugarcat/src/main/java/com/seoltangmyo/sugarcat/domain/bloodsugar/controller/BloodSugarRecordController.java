6c32b908d3b3578f891cff306a72605fad266c0c
5ranke <wjdtnwls0106@gmail.com>
2026-05-11 23:27:48 +0900

    feat: 혈당 기록 Controller 추가

---
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/bloodsugar/controller/BloodSugarRecordController.java
@@ -0,0 +1,13 @@
+package com.seoltangmyo.sugarcat.domain.bloodsugar.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/blood-sugar-records")
+@RequiredArgsConstructor
+public class BloodSugarRecordController {
+
+
+}
