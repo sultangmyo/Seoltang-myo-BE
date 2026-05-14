26e2bef73f87cc1efc75953dec9ae3dfe5b399be
5ranke <wjdtnwls0106@gmail.com>
2026-05-14 23:55:23 +0900

    feat: 혈당 기록 Service 저장 로직 구현

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/bloodsugar/service/impl/BasicBloodSugarRecordService.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/bloodsugar/service/impl/BasicBloodSugarRecordService.java
@@ -2,8 +2,13 @@ package com.seoltangmyo.sugarcat.domain.bloodsugar.service.impl;
 
 import com.seoltangmyo.sugarcat.domain.bloodsugar.dto.BloodSugarRecordCreateRequest;
 import com.seoltangmyo.sugarcat.domain.bloodsugar.dto.BloodSugarRecordCreateResponse;
+import com.seoltangmyo.sugarcat.domain.bloodsugar.entity.BloodSugarRecord;
+import com.seoltangmyo.sugarcat.domain.bloodsugar.entity.SugarStatus;
 import com.seoltangmyo.sugarcat.domain.bloodsugar.repository.BloodSugarRecordRepository;
 import com.seoltangmyo.sugarcat.domain.bloodsugar.service.BloodSugarRecordService;
+import com.seoltangmyo.sugarcat.domain.cat.entity.Cat;
+import com.seoltangmyo.sugarcat.domain.user.entity.User;
+import com.seoltangmyo.sugarcat.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +18,36 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class BasicBloodSugarRecordService implements BloodSugarRecordService {
     private final BloodSugarRecordRepository bloodSugarRecordRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public BloodSugarRecordCreateResponse createBloodSugarRecord(UUID userId, BloodSugarRecordCreateRequest request) {
-        return null;
+    public BloodSugarRecordCreateResponse createBloodSugarRecord(
+            UUID userId,
+            BloodSugarRecordCreateRequest request
+    ) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        Cat cat = user.getCat();
+
+        SugarStatus sugarStatus = SugarStatus.from(request.sugarValue());
+
+        BloodSugarRecord bloodSugarRecord = BloodSugarRecord.builder()
+                .cat(cat)
+                .recordedBy(user)
+                .recordDate(request.recordedDate())
+                .recordTime(request.recordedTime())
+                .sequence(request.sequence())
+                .sugarValue(request.sugarValue())
+                .sugarStatus(sugarStatus)
+                .build();
+
+        bloodSugarRecordRepository.save(bloodSugarRecord);
+
+        return new BloodSugarRecordCreateResponse(
+                bloodSugarRecord.getBloodSugarRecordId(),
+                bloodSugarRecord.getSugarStatus()
+        );
     }
 
 }
