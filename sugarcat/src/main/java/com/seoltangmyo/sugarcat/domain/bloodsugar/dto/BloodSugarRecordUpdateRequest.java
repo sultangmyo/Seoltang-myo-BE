c069784ee67a074ea404554f3aa7fb5d35c3d883
meaningGitt <a0104037a@gmail.com>
2026-06-03 13:20:00 +0900

    feat : BloodSugar/Meal/Insulin DTO에 날짜/시간 포맷 명시

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/bloodsugar/dto/BloodSugarRecordUpdateRequest.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/bloodsugar/dto/BloodSugarRecordUpdateRequest.java
@@ -1,11 +1,12 @@
 package com.seoltangmyo.sugarcat.domain.bloodsugar.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record BloodSugarRecordUpdateRequest(
-        LocalDate recordedDate,
-        LocalTime recordedTime,
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate recordedDate,
+        @JsonFormat(pattern = "HH:mm") LocalTime recordedTime,
         int sequence,
         int sugarValue
 ) {
