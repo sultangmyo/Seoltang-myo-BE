f745afdeafbf6f1923e30773f797feeda1283d2e
5ranke <wjdtnwls0106@gmail.com>
2026-06-05 09:52:47 +0900

    feat: import문 추가

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/meal/repository/MealRecordRepository.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/meal/repository/MealRecordRepository.java
@@ -5,6 +5,7 @@ import com.seoltangmyo.sugarcat.domain.meal.entity.MealRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface MealRecordRepository extends JpaRepository<MealRecord, UUID> {
