bb0b0aa4e50e859d7a296ff40d586fc793e2ea04
5ranke <wjdtnwls0106@gmail.com>
2026-07-14 14:00:35 +0900

    style: import문 삭제

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/schedule/service/CareScheduleService.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/schedule/service/CareScheduleService.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.UUID;
 
 @Service
