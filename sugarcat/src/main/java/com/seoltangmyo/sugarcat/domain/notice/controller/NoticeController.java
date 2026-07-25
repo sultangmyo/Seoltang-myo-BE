5e6e2591a1c4c723717a235298ab99eb122056e1
meaningGitt <a0104037a@gmail.com>
2026-07-25 13:58:45 +0900

    feat:공지 스웨거 추가

--- sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/notice/controller/NoticeController.java
+++ sugarcat/src/main/java/com/seoltangmyo/sugarcat/domain/notice/controller/NoticeController.java
@@ -6,11 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Notice", description = "공지사항 API")
 @RestController
 @RequestMapping("/api/v1/notices")
 public class NoticeController {
 
+    @Operation(summary = "공지 활성화")
     @GetMapping("/active")
     public ResponseEntity<NoticeResponse> getActiveNotice() {
         return ResponseEntity.ok(NoticeContent.activeNotice());
